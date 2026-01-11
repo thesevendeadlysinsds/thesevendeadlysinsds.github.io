8b7c911f676d258e347e1d5db87c4945e6bb6387
thesevendeadlysinsds <139150860+thesevendeadlysinsds@users.noreply.github.com>
2026-01-11 18:45:13 +0100

    Update gtn_file.py

--- py/txt/gtn_file.py
+++ py/txt/gtn_file.py
@@ -1,3 +1,5 @@
+import requests
+
 def transform_to_int(tifsec):
     autorises = "".join([chr(i) for i in range(ord("0"), ord("9")+1)]) + "+-*/%(). "
     # print(tifsec)
@@ -18,3 +20,21 @@ def open_web(url):
     except:
         return f"Erreur lors de la récupération : {e}"
 # print(open_web("https://thesevendeadlysinsds.github.io/py/txt/gtn-data1.txt"))
+
+def toto(file_name):
+    is_web = file_name.startswith("http://") or file_name.startswith("https://")
+    if is_web:
+        f = open_web(file_name)
+    else:
+        f = open(file_name, "r", encoding="utf-8")
+    dico = {}
+    for i in f if not is_web else f.strip().split("\n"):
+        a = i.split()
+        # print(a)
+        if len(a) >= 3:
+            dico[a[0]] = {"borne1": transform_to_int(a[1]), "borne2": transform_to_int(a[2])}
+    if not is_web:
+        f.close()
+    return dico
+# print(toto(str(path_name).replace("\\", "/") + "/gtn_data.txt"))
+# print(toto("http://thesevendeadlysinsds.github.io/py/txt/gtn-data1.txt"))
